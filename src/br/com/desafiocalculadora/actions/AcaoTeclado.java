44b89b366b9b3749d545004a8119060aeaf950d6
Marcos Ribeiro <marcossrj90@gmail.com>
2018-06-28 21:39:58 -0300

    Aprimorando visor para acumular numeros com teclado

--- src/br/com/desafiocalculadora/actions/AcaoTeclado.java
+++ src/br/com/desafiocalculadora/actions/AcaoTeclado.java
@@ -37,25 +37,25 @@ public class AcaoTeclado implements KeyListener {
 		if(e.getKeyCode() == 16) {
 			aux = 0;
 		} else if(aux == 0 && e.getKeyCode() == 48 || e.getKeyCode() == 96) {
-			edit.setText("0");
+			formataZero();
 		}else if(aux == 0 && e.getKeyCode() == 49 || e.getKeyCode() == 97) {
-			edit.setText("1");
+			formataUm();
 		} else if(aux == 0 && e.getKeyCode() == 50 || e.getKeyCode() == 98) {
-			edit.setText("2");
+			formataDois();
 		} else if(aux == 0 && e.getKeyCode() == 51 || e.getKeyCode() == 99) {
-			edit.setText("3");
+			formataTres();
 		} else if(aux == 0 && e.getKeyCode() == 52 || e.getKeyCode() == 100) {
-			edit.setText("4");
+			formataQuatro();
 		} else if(aux == 0 && e.getKeyCode() == 53 || e.getKeyCode() == 101) {
-			edit.setText("5");
+			formataCinco();
 		} else if(aux == 0 && e.getKeyCode() == 54 || e.getKeyCode() == 102) {
-			edit.setText("6");
+			formataSeis();
 		} else if(aux == 0 && e.getKeyCode() == 55 || e.getKeyCode() == 103) {
-			edit.setText("7");
+			formataSete();
 		} else if(aux == 0 && e.getKeyCode() == 56 || e.getKeyCode() == 104) {
-			edit.setText("8");
+			formataOito();
 		} else if(aux == 0 && e.getKeyCode() == 57 || e.getKeyCode() == 105) {
-			edit.setText("9");
+			formataNove();
 		} else if(aux == 1 && e.getKeyCode() == 61 || e.getKeyCode() == 107) {
 			edit.setText("+");
 		} else if(aux == 0 && e.getKeyCode() == KeyEvent.VK_SLASH || e.getKeyCode() == 111 || e.getExtendedKeyCode() == 47) {
@@ -73,6 +73,87 @@ public class AcaoTeclado implements KeyListener {
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	//Metodos para acrescentar numeros quando digitados
+	public void formataZero() {
+		if(edit.getText().equals("0")) {
+			edit.setText("0,");
+		} else {
+			edit.setText(edit.getText() + "0");
+		}
+	}
+	
+	public void formataUm() {
+		if(edit.getText().equals("0")) {
+			edit.setText("1");
+		} else {
+			edit.setText(edit.getText() + "1");
+		}
+	}
+	
+	public void formataDois() {
+		if(edit.getText().equals("0")) {
+			edit.setText("2");
+		} else {
+			edit.setText(edit.getText() + "2");
+		}
+	}
+	
+	public void formataTres() {
+		if(edit.getText().equals("0")) {
+			edit.setText("3");
+		} else {
+			edit.setText(edit.getText() + "3");
+		}
+	}
+	
+	public void formataQuatro() {
+		if(edit.getText().equals("0")) {
+			edit.setText("4");
+		} else {
+			edit.setText(edit.getText() + "4");
+		}
+	}
+	
+	public void formataCinco() {
+		if(edit.getText().equals("0")) {
+			edit.setText("5");
+		} else {
+			edit.setText(edit.getText() + "5");
+		}
+	}
+	
+	public void formataSeis() {
+		if(edit.getText().equals("0")) {
+			edit.setText("6");
+		} else {
+			edit.setText(edit.getText() + "6");
+		}
+	}
+	
+	public void formataSete() {
+		if(edit.getText().equals("0")) {
+			edit.setText("7");
+		} else {
+			edit.setText(edit.getText() + "7");
+		}
+	}
+	
+	public void formataOito() {
+		if(edit.getText().equals("0")) {
+			edit.setText("8");
+		} else {
+			edit.setText(edit.getText() + "8");
+		}
+	}
+	
+	public void formataNove() {
+		if(edit.getText().equals("0")) {
+			edit.setText("9");
+		} else {
+			edit.setText(edit.getText() + "9");
+		}
 	}
 
 }
